f7206beb66edb3f0a44700ef225a45d0efb3f4f2
James Abustan <JimmyJammies@Jamess-MacBook-Pro.local>
2020-06-08 21:19:54 +0100

    scheduler component

--- frontend/src/components/donor_dashboard.js
+++ frontend/src/components/donor_dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './navbar_dashboard';
+import Scheduler from './scheduler';
 
 class DonorDashboard extends React.Component {
 
@@ -11,6 +12,7 @@ class DonorDashboard extends React.Component {
                 <h1>Hello im Donor Dashboard</h1>
                 <h2>Donor Profile</h2>
                 <h2>Booking / Scheduling appt</h2>
+            < Scheduler />
                 <h2>Friends / nudge</h2>
                 <h2>Map</h2>
                 </div>
