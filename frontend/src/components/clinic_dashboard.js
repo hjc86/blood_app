ed730e98a9db79154b7ea7ddcc0ade50a1910f3e
James Abustan <JimmyJammies@Jamess-MacBook-Pro.local>
2020-06-08 15:44:05 +0100

    dash changes

--- frontend/src/components/clinic_dashboard.js
+++ frontend/src/components/clinic_dashboard.js
@@ -6,6 +6,8 @@ class ClinicDashboard extends React.Component {
         return (
    
             <h1>Hello im ClinicDashboard</h1>
+            <h2>See appointments for today</h2>
+            <h3>validate feature to check in the donor</h3>
 
         )
     }
--- frontend/src/components/donor_dashboard.js
+++ frontend/src/components/donor_dashboard.js
@@ -5,6 +5,10 @@ class DonorDashboard extends React.Component {
     render() {
         return (
             <h1>Hello im Donor Dashboard</h1>
+            <h2>Donor Profile</h2>
+            <h2>Booking / Scheduling appt</h2>
+            <h2>Friends / nudge</h2>
+            <h2>Map</h2>
         )
     }
 }
